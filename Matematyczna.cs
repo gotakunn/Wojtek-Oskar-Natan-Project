4e3584a379c3ffefa5c2528763f0716f8744e48e
natan . <gotanxd@gmail.com>
2025-10-03 15:22:42 +0200

    dodalem NWD

--- Matematyczna.cs
+++ Matematyczna.cs
@@ -2,5 +2,19 @@ namespace ConsoleApp1;
 
 public class Matematyczna
 {
+    static int NWD(int a, int b)
+    {
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+        if (a == 0) return b;
+        if (b == 0) return a;
 
+        while (b != 0)
+        {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
 }
