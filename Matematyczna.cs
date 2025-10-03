36664027ef40857ce09450b8c3cbcf53affcb5cc
NocnyRest <zespolzip@menex.pl>
2025-10-03 15:35:29 +0200

    Dodano sortoesnir bśbrlkoer

--- Matematyczna.cs
+++ Matematyczna.cs
@@ -1,6 +1,24 @@
-namespace ConsoleApp1;
+﻿namespace ConsoleApp1;
 
 public class Matematyczna
 {
 
+    public int[] BoobleSort(int[] tab)
+    {
+        int n = tab.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (tab[j] > tab[j + 1])
+                {
+                    // swap temp and arr[i]
+                    int temp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+                }
+            }
+        }
+        return tab;
+    }
 }
