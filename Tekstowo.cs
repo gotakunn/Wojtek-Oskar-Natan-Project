fd8c8fa474c6520ddcfc22c717461c4fd9c23cc3
SeRyKii <kop-woj@wp.pl>
2025-10-03 15:32:23 +0200

    Dodano szyfr cezara

--- Tekstowo.cs
+++ Tekstowo.cs
@@ -19,4 +19,24 @@ public class Tekstowo
 
         return true;
     }
+
+    public string SzyfrCezara(string tekst, int przesuniecie)
+    {
+        char[] zaszyfrowanyTekst = new char[tekst.Length];
+
+        for (int i = 0; i < tekst.Length; i++)
+        {
+            char znak = tekst[i];
+
+            if (char.IsLetter(znak))
+            {
+                char baza = char.IsUpper(znak) ? 'A' : 'a';
+                znak = (char)((((znak + przesuniecie) - baza) % 26) + baza);
+            }
+
+            zaszyfrowanyTekst[i] = znak;
+        }
+
+        return new string(zaszyfrowanyTekst);
+    }
 }
