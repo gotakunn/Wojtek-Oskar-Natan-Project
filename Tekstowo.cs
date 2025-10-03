61ad5058058c29fd158148613858cd912a4bff6e
SeRyKii <kop-woj@wp.pl>
2025-10-03 15:25:11 +0200

    Zaimplementowano CzyPalindrom

--- Tekstowo.cs
+++ Tekstowo.cs
@@ -2,5 +2,21 @@ namespace ConsoleApp1;
 
 public class Tekstowo
 {
+    public bool CzyPalindrom(string tekst)
+    {
+        int lewy = 0;
+        int prawy = tekst.Length - 1;
 
+        while (lewy < prawy)
+        {
+            if (tekst[lewy] != tekst[prawy])
+            {
+                return false;
+            }
+            lewy++;
+            prawy--;
+        }
+
+        return true;
+    }
 }
